1bd7fb1ebf8628c3f9ad4f793cc67c800a0991e0
dennisochieng <ochiengdennis32@gmail.com>
2025-04-30 13:00:17 +0300

    introduced button functionality

--- wick/src/Components/Navbar.jsx
+++ wick/src/Components/Navbar.jsx
@@ -10,3 +10,26 @@ function Navbar({ isSignedUp, setIsSignedUp }) {
     setIsSignedUp(false);
     Swal.fire('Logged Out', 'You have successfully logged out.', 'success');
   };
+
+  return (
+    <nav className="navbar">
+      <div>
+        <h1 className="title">Wick & Whistle Pantry</h1>
+      </div>
+      <ul className="nav-links">
+        <Link to="/" className="nav-button">Home</Link>
+        <Link to="/favorites" className="nav-button">My Favorites</Link>
+        {!isSignedUp && (
+          <Link to="/signup" className="nav-button">
+            Sign Up
+          </Link>
+        )}
+        {isSignedUp && ( 
+          <button onClick={handleLogout} className="small-button">Logout</button>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
