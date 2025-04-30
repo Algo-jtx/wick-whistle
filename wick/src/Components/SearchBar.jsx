81d74db1b27617ed769d4c2405f0e1287fce39eb
ian <ianwisdom90@gmail.com>
2025-04-30 11:20:51 +0300

    created a function searchbar

---
+++ wick/src/Components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function SearchBar ({ searchTerm, setSearchTerm }) 
+//   return (
+//     <div className="search-bar">
+//       <input
+//         type="text"
+//         placeholder="Search for delicious meals"
+//         value={searchTerm}
+//         onChange={(e) => setSearchTerm(e.target.value)}
+//         className = "search-input"
+//       />
+//       {/* <button className="search-button">search</button> */}
+
+//     </div>
+//   );
+// }
+// export default SearchBar;
