ec623ed76e007e1fef47c3ee4240ec61a4c6d89d
ian <ianwisdom90@gmail.com>
2025-04-30 11:25:51 +0300

    placed content in the place holder

--- wick/src/Components/SearchBar.jsx
+++ wick/src/Components/SearchBar.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function SearchBar ({ searchTerm, setSearchTerm }) {
   return (
     <div className="search-bar">
-//       <input
-//         type="text"
-//         placeholder="Search for delicious meals"
-//         value={searchTerm}
+      <input
+        type="text"
+        placeholder="Search for delicious meals"
+        value={searchTerm}
 //         onChange={(e) => setSearchTerm(e.target.value)}
 //         className = "search-input"
 //       />
